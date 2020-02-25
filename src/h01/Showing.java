f033edac6ab147f1b381645345909f2ac6c34a12
Ricardoschrandt <Ricardonschrandt@gmail.com>
2020-02-25 13:42:31 +0100

    Tekst verandering

---
+++ src/h01/Showing.java
@@ -0,0 +1,19 @@
+//Voorbeeld 4.1
+
+import java.awt.*;
+import java.applet.*;
+
+public class Showing extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.blue);
+        g.setColor(Color.yellow);
+        g.drawLine(30,30, 60, 60);
+        g.drawLine(30, 30, 0, 60);
+        g.drawLine(0, 60, 60, 60);
+
+    }
+}
