53003a316b571ab46b7323ff4b38f718bb86182c
Ricardoschrandt <Ricardonschrandt@gmail.com>
2020-02-26 10:22:58 +0100

    Tekst verandering

---
+++ src/h4/NLvlag.java
@@ -0,0 +1,16 @@
+package h4;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class NLvlag extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.black);
+        g.setColor(Color.white);
+    }
+
+}
---
+++ src/h4/Showing.java
@@ -0,0 +1,23 @@
+package h4;//Voorbeeld 4.1
+
+import java.awt.*;
+import java.applet.*;
+
+public class Showing extends Applet {
+
+    public void init() {
+    }
+
+        public void paint(Graphics g) {
+            setBackground(Color.blue);
+            g.setColor(Color.BLACK);
+        g.drawLine(30, 30, 60, 60);
+        g.drawLine(30, 30, 0, 60);
+        g.drawLine(0, 60, 60, 60);
+        g.drawRect(0, 60, 60, 60);
+        g.drawRect(10, 75, 15, 15);
+        g.drawRect(30, 90, 15, 30);
+    }
+
+
+}
