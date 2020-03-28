b661341d2255b180bd604f85341c4846767108cb
Ricardoschrandt <Ricardonschrandt@gmail.com>
2020-03-28 13:12:20 +0100

    h11 opdracht 6 af

---
+++ src/h11/opdracht1.java
@@ -0,0 +1,30 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class opdracht1 extends Applet {
+
+
+    public void init() {}
+
+
+
+    public void paint(Graphics g) {
+        int teller;
+        int x, y, lengte, breedte ;
+        x  = 60;
+        y = 60;
+        lengte = 25;
+        breedte = 25;
+
+       for (teller = 0; teller < 5;) {
+            y += -6;
+            x += -6;
+            breedte += 12;
+            lengte += 12;
+            g.drawOval(x , y, lengte, breedte );
+            teller++;
+        }
+    }
+}
