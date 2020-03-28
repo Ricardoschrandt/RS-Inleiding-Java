f98b77cb7ea67b09c26d3e3fedfc7e0174d55663
Ricardoschrandt <Ricardonschrandt@gmail.com>
2020-03-28 14:24:47 +0100

    h11 opdracht 7 af

---
+++ src/h11/Opdracht1.java
@@ -0,0 +1,29 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht1 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+        g.drawLine(20, 20, 100, 20);
+        g.drawLine(20, 30, 100, 30);
+        g.drawLine(20, 40, 100, 40);
+        g.drawLine(20, 50, 100, 50);
+        g.drawLine(20, 60, 100, 60);
+        g.drawLine(20, 70, 100, 70);
+        g.drawLine(20, 80, 100, 80);
+        g.drawLine(20, 90, 100, 90);
+        g.drawLine(20, 100, 100,100);
+        g.drawLine(20, 110, 100, 110);
+
+
+
+
+    }
+}
---
+++ src/h11/Opdracht2.java
@@ -0,0 +1,30 @@
+package h11;
+
+
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht2 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+        g.drawString("10", 20, 30);
+        g.drawString("11", 20, 50);
+        g.drawString("12", 20, 70);
+        g.drawString("13", 20, 90);
+        g.drawString("14", 20, 110);
+        g.drawString("15", 20, 130);
+        g.drawString("16", 20, 150);
+        g.drawString("17", 20, 170);
+        g.drawString("18", 20, 190);
+        g.drawString("19", 20, 210);
+        g.drawString("20", 20, 230);
+
+
+    }
+}
