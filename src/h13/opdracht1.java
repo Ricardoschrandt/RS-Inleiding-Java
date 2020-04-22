b853ac7bc086cfca91aeda1f37d3e3c4bc2ef084
31627 <Ricardonschrandt@gmail.com>
2020-04-22 10:20:34 +0200

    h13 done

--- src/h13/opdracht1.java
+++ src/h13/opdracht1.java
@@ -1,4 +1,22 @@
 package h13;
 
-public class opdracht1 applet {
+import java.applet.Applet;
+import java.awt.*;
+
+public class opdracht1 extends Applet {
+    public void init() {
+
+    }
+
+    void tekenDriehoek(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
+        g.drawLine(x1,y1,x2,y2);
+        g.drawLine(x2,y2,x3,y3);
+        g.drawLine(x3,y3,x1,y1);
+
+    }
+
+    public void paint(Graphics g) {
+        tekenDriehoek(g,100,150,150,150,125,100);
+
+    }
 }
--- src/h13/praktijkopdracht.java
+++ src/h13/praktijkopdracht.java
@@ -1,4 +1,63 @@
 package h13;
 
-public class praktijkopdracht {
+import java.applet.Applet;
+import java.awt.*;
+
+public class praktijkopdracht extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        boomTekenen(g);
+
+    }
+
+    public void boomTekenen(Graphics g) {
+
+        Color Bruin = new Color(153, 102, 0);
+
+        Color Groen = Color.green;
+        Color boomstam = Bruin;
+
+        int bladsize = 80;
+        int x = 20;
+        int y = 50;
+        int hoogte = 120;
+        int breedte = 50;
+        int boomstamX = 35;
+        int boomstamY = 110;
+
+        for (int bosteller = 0; bosteller < 2; bosteller++) {
+            //rij 1
+            for (int rijteller = 0; rijteller < 5; rijteller++) {
+                g.setColor(boomstam);
+                g.fillRect(boomstamX, boomstamY, breedte, hoogte);
+                g.setColor(Groen);
+                g.fillOval(x, y, bladsize, bladsize);
+
+                x += bladsize;
+                boomstamX += bladsize;
+            }
+            //rij 2
+            for (int tweederij = 0; tweederij < 4; tweederij++) {
+                x = -15;
+                y = 80;
+                boomstamX = -5;
+                boomstamY = 140;
+
+                g.setColor(boomstam);
+                g.fillRect(boomstamX, boomstamY, breedte, hoogte);
+                g.setColor(Groen);
+                g.fillOval(x, y, bladsize, bladsize);
+
+                x += bladsize;
+                boomstamX += bladsize;
+                y += 30;
+            }
+
+
+        }
+    }
 }
